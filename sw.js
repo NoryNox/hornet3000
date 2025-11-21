b44505b50933b8adcfa4793afe393e44a165ee47
NoryNox <225153648+NoryNox@users.noreply.github.com>
2025-11-21 14:28:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-const CACHE = 'hornet3000-offline-v1';
-const FILES = [
-  '/', 'index.html', 'hornet.html', 'manifest.json',
-  'icon-192.png', 'icon-512.png'
-];
+const CACHE = 'beekeeper-v1';
+const FILES = ['/', 'index.html', 'manifest.json', 'assets/icon.png', 'hornisse.html', 'varroa.html', 'drohnenbrut.html', 'pollen.html', 'schwarm.html', 'queen.html', 'js/config.js', 'js/camera.js', 'js/model-loader.js', 'js/alarm.js', 'js/ui.js', 'js/modes/hornisse.js', 'js/modes/varroa.js', 'js/modes/drohnenbrut.js', 'js/modes/pollen.js', 'js/modes/schwarm.js', 'js/modes/queen.js'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
